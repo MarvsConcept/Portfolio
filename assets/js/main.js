5287da19c33441be6e7e5342219f5419b4a7856c
MarvsConcept <Marvade8@gmail.com>
2023-12-28 23:06:13 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,25 +31,7 @@ modalClose.forEach((mc) =>{
 
 
 
-/*=============== MIXITUP FILTER PORTFOLIO ===============*/
-let mixerPortfolio = mixitup('.work__container', {
-  selectors: {
-    target: '.work__card'
-  },
-  animation: {
-    duration: 300
-  }
-});
 
-/* Link active work */ 
-const linkWork = document.querySelectorAll('.work__item')
-
-function activeWork(){
-  linkWork.forEach(l=> l.classList.remove('active-work'))
-  this.classList.add('active-work')
-}
-
-linkWork.forEach(l=> l.addEventListener('click', activeWork))
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
@@ -136,3 +118,24 @@ const sr = ScrollReveal({
 sr.reveal('.home__data')
 sr.reveal('.home__handle', {delay:700})
 sr.reveal('.home__social, .home__scroll', {delay:900, origin: 'bottom'})
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+    target: '.work__card'
+  },
+  animation: {
+    duration: 300
+  }
+});
+
+/* Link active work */ 
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+  linkWork.forEach(l=> l.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
